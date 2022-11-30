d398f6d7b88ac73cc56b1264f41e1f71516ef851
Lisa Lewis <mrs.lisalewis@outlook.com>
2022-11-30 13:26:29 +0800

    Add project files.

---
+++ TodoAPI/Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TodoAPI.Models;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    { }
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+
+}
+
